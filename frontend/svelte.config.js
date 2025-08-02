0e17afe30f70fa8a2af6e4928fcdfff2959cb64d
zazhiladhf <zazhil24@gmail.com>
2025-08-02 11:37:15 +0700

    add docker compose

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,13 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+import adapter from '@sveltejs/adapter-node';
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ fallback: 'index.html' })
+		adapter: adapter()
 	}
 };
-
-export default config;
